7c92ecb74b0cc3c0499dc9127fb6962cf477474c
Nemes <sangawenemes@gmail.com>
2026-06-06 21:59:49 +0300

    initial commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use Illuminate\Validation;
 
 
 class ProductController extends Controller
@@ -38,7 +39,7 @@ public function store(Request $request)
             'data' => $product
         ], 201);
 
-    } catch (\Illuminate\Validation\ValidationException $e) {
+    } catch (\ValidationException $e) {
         return response()->json([
             'success' => false,
             'message' => 'Validation failed',
@@ -57,12 +58,78 @@ public function store(Request $request)
 
 public function update(Request $request, $id)
 {
-// 
+    try  {
+        $product = Product::findOrFail($id);
+
+        if (!$product) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Product not found'
+            ], 404);
+        
+    }
+
+    $validated = $request->validate([
+        'name' => 'required|string|max:255',
+        'description' => 'nullable|string',
+        'price' => 'required|numeric|min:0',
+        'quantity' => 'required|integer|min:0',
+    ]);
+
+    $product->update($validated);
+
+    return response()->json([
+        'success' => true,
+        'message' => 'Product updated successfuly',
+        'data' => $product
+    ]);
+
+
+
+}catch (ValidationException $e) {
+    return response()->json([
+        'message' => false,
+        'message' => 'Validation failed',
+        'errors' => $e->errors()
+    ], 422);
+
+} catch (\Exception $e) {
+    return response()->json([
+        'success' => false,
+        'message' => 'Failed to update product',
+        'error' => $e->getMessage()
+    ], 500);
+}
 }
 
 public function destroy($id)
 {
-// 
+ try  {
+        $product = Product::findOrFail($id);
+
+        if (!$product) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Product not found'
+            ], 404);
+        
+    }
+
+    $product->delete($id);
+
+    return response()->json([
+        'success' => true,
+        'message' => 'Product updated successfuly',
+        
+    ]);
+
+} catch (\Exception $e) {
+    return response()->json([
+        'success' => false,
+        'message' => 'Failed to delete product',
+        'error' => $e->getMessage()
+    ], 500);
+}
 }
 
 }
